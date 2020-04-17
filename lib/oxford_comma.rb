5d5a71135c295d06bbafb21c950a093eccdb3776
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-17 16:36:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if (array.size > 2)
+    word =  "and " + array[array.size-1]
+    array[array.size-1]  = word
+    (array.join(", "))
+  elsif  (array.size > 1)
+    word =  " and " + array[array.size-1]
+    array[array.size-1]  = word
+    array.join
+  else
+    array.join
+  end
 end
